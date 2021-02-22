7ed1cd118a873186f7d391425dd2ead072021f8d
Robin Singla <robin.singla@asahiindia.com>
2021-02-22 22:51:28 +0530

    Initial commit

---
+++ app/src/main/java/com/example/imageslider/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.imageslider;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
+import com.smarteist.autoimageslider.SliderAnimations;
+import com.smarteist.autoimageslider.SliderView;
+
+public class MainActivity extends AppCompatActivity {
+
+    SliderView sliderView;
+    int[] images = {R.drawable.one,
+    R.drawable.two,
+    R.drawable.three,
+    R.drawable.four,
+    R.drawable.five,
+    R.drawable.six};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sliderView = findViewById(R.id.image_slider);
+
+        SliderAdapter sliderAdapter = new SliderAdapter(images);
+
+        sliderView.setSliderAdapter(sliderAdapter);
+        sliderView.setIndicatorAnimation(IndicatorAnimationType.WORM);
+        sliderView.setSliderTransformAnimation(SliderAnimations.DEPTHTRANSFORMATION);
+        sliderView.startAutoCycle();
+
+    }
+}
